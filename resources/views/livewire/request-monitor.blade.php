8840a1e6beb7f369b7c1465f58c78d7c685f184d
Victor Ndu <d.pamekar@gmail.com>
2026-02-19 09:34:15 +0100

    perf(ui): use wire:poll.visible to only poll when the tab is active

--- resources/views/livewire/request-monitor.blade.php
+++ resources/views/livewire/request-monitor.blade.php
@@ -1,4 +1,4 @@
-<div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8" wire:poll.5s>
+<div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8" wire:poll.5s.visible>
     <x-slot name="header">
         <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
